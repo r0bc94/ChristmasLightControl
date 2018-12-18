1b647fc1e161df3db3654b884f3695446fdcc2ab
r0bc94 <robin.pal@hotmail.de>
2018-12-19 00:17:55 +0100

    :bug: inherit gpiodevice from device

--- src/gpio_device.py
+++ src/gpio_device.py
@@ -1,9 +1,10 @@
+from .device import Device
 """
 A GPIO device is a simple device which can be turned on and off by 
 setting a designated GPIO pin.
 """
 
-class GPIODevice():
+class GPIODevice(Device):
     def __init__(self, name: str, pin: int):
         super().__init__(name)
         self.__pin = pin
