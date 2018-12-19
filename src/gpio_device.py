c0f633087e30b0c6d363de8a962eef10336ae617
r0bc94 <robin.pal@hotmail.de>
2018-12-19 22:38:15 +0100

    :sparkles: implement turnOn and turnOff methods

--- src/gpio_device.py
+++ src/gpio_device.py
@@ -1,4 +1,7 @@
+import RPi.GPIO as GPIO
+
 from .device import Device
+
 """
 A GPIO device is a simple device which can be turned on and off by 
 setting a designated GPIO pin.
@@ -8,11 +11,20 @@ class GPIODevice(Device):
     def __init__(self, name: str, pin: int):
         super().__init__(name)
         self.__pin = pin
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
     
     def getPin(self):
         return self.__pin
     
     def setPin(self, pin: int):
         self.__pin = pin
+
+    def turnOn(self):
+        GPIO.output(self.__pin, GPIO.HIGH)
+    
+    def turnOff(self):
+        GPIO.output(self.__pin, GPIO.LOW)
 
     pin = property(getPin, setPin)
