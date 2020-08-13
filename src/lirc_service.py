b1fb5e7fae81cfafdb1b6b116c9676fa00675585
robin <robin.pal@hotmail.de>
2020-08-14 01:36:12 +0200

    :bug: Fix No Such File or Directory Error

--- src/lirc_service.py
+++ src/lirc_service.py
@@ -2,22 +2,26 @@ import logging
 import coloredlogs
 import subprocess
 
-logger = logging.getLogger().getChild("lirc_service")
-coloredlogs.install()
+logger = logging.getLogger("lirc_service")
+coloredlogs.install(level='DEBUG', logger=logger)
 
 def runLircCommand(remoteName: str, key: str, repeatCount=1, timeout=2):
     irSendCommand = f'irsend send_once {remoteName} {key} --count {repeatCount}'
     logger.debug(f'Running Command: {irSendCommand}')
 
     try:
-        process = subprocess.run(irSendCommand, timeout=timeout)
+        subprocess.run(irSendCommand, timeout=timeout, capture_output=True, shell=True)
     except subprocess.TimeoutExpired as timeout: 
+        print('bla')
         logger.warning('The executed irsend command took to long to respond')
         logger.warning('Depending on your configuration, this may be normal')
         logger.warning('especially when using a repeat count > 1.')
         logger.warning(f'IRSend Output: {timeout.output}')
     except subprocess.CalledProcessError as err:
+        print('blub')
         logger.error('Error while executing the irsend process.')
         logger.error('This may caused due to a missing lirc installation or missconfiguration')
         logger.error(f'IRSend Output: {err.output}')
-        
+    except BaseException as ex:
+        logger.error('Unknown Error while executing the irsend process.')
+        logger.exception(ex)
